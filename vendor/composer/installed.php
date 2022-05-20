84360377b6c2ddacac66aca1bafa3290e13f32a8
AndryMSI3 <andryandryy@gmail.com>
2022-05-20 09:57:58 +0300

    The project with database and php

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8afbafaaeb6cd6e354d202f17fdefa9cc2611e12',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8afbafaaeb6cd6e354d202f17fdefa9cc2611e12',
+    ),
+  ),
+);
