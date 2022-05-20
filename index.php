510daa23dec4e9dee383c4fbbbbfaa2a93a8ad6d
AndryMSI3 <andryandryy@gmail.com>
2022-05-20 09:59:33 +0300

    Update few file

--- index.php
+++ index.php
@@ -172,5 +172,17 @@
         <script src="/js/modal.js"></script>
     </body>
 </html>
-
+<?php
+    require 'vendor/autoload.php';
+    use PostgreSQLTutorial\Connection as Connection;
+    try 
+    {
+        Connection::get()->connect();
+        echo 'A connection to the PostgreSQL database sever has been established successfully';
+    } 
+    catch (\PDOException $e) 
+    {
+    echo $e->getMessage();
+    }
+?>
 
