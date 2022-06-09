c5ecbf21ac1b8daca0f3b59d2d52612e09e7be3e
AndryMSI3 <andryandryy@gmail.com>
2022-06-09 10:27:06 +0300

    test

--- app/Suscribe.php
+++ app/Suscribe.php
@@ -214,7 +214,7 @@
         $charactersLength = strlen($characters);
         $randomString = '';
         for ($i = 0; $i < $length; $i++) {
-            $randomString .= $characters[rand(0, $charactersLength - 1)];
+            $randomString .= $characters[rand(0, ($charactersLength - 1))];
         }
         return $randomString;
     }
