fc2a4cc1b5f82093cd43d8932ad94e1b55a5484b
AndryMSI3 <andryandryy@gmail.com>
2022-06-09 17:16:23 +0300

    test

--- app/Suscribe.php
+++ app/Suscribe.php
@@ -173,6 +173,7 @@
         $stmt_max_id = $pdo->prepare($sql_last_id);
         $stmt_max_id->execute();     
         $id = $stmt_max_id->fetch();
+        echo $id[0];
         var_dump($id[0]);
         // execute the insert           
         // Get the API client and construct the service object.
