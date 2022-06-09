3542b36ab2ed75ec30dc5e43d73618cde0c17972
AndryMSI3 <andryandryy@gmail.com>
2022-06-09 20:23:12 +0300

    test

---
+++ index2.php
@@ -0,0 +1,30 @@
+<?php
+    require 'vendor/autoload.php';
+    use PostgreSQLTutorial\Connection as Connection;
+    class SpecificException extends Exception{}
+    // /* Execute this first before launching the main page */
+    try 
+    {
+        $conn = Connection::get()->connect();
+        $sql = 'CREATE TABLE IF NOT EXISTS users
+        (
+            id SERIAL NOT NULL,
+            identifiant character varying(255) ,
+            _password character varying(255),
+            gender bit(1),
+            birthdate date,
+            lastname character varying(255),
+            firstname character varying(255),
+            confirmkey character varying(255),
+            confirm boolean DEFAULT false,
+            CONSTRAINT id PRIMARY KEY (id)
+        )';
+        $statement = $conn->prepare($sql);
+        $statement->execute();
+        echo "The database was created successfully!";
+    } 
+    catch (\PDOException $e) 
+    {
+    echo $e->getMessage();
+    }
+?>
